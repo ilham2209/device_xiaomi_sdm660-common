e2dda3fa9be33b65394cd68d4e69c2cdaaf1f8a5
narjirolu125 <narjirolu@gmail.com>
2020-05-29 16:33:27 +0000

    Revert "sdm660-common: power-libperfmgr: Follow symlinks when writing to dt2w toggle path"
    This reverts commit 2bebfeb63c758644bc5e67f8d2b884d0db2768c9.

--- power-libperfmgr/Power.cpp
+++ power-libperfmgr/Power.cpp
@@ -229,6 +229,7 @@ Return<void> Power::setFeature(Feature feature, bool activate)  {
 #ifdef TAP_TO_WAKE_NODE
         case Feature::POWER_FEATURE_DOUBLE_TAP_TO_WAKE:
             ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE);
+            ::android::base::WriteStringToFile(activate ? "1" : "0", TAP_TO_WAKE_NODE2);
             break;
 #endif
         default:
